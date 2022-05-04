d32c113734d2f110535d8f326de3b6cc49edb160
5254416 <100807837+5254416@users.noreply.github.com>
2022-05-04 17:26:23 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
+#include "lib.h"
 
+float max (float num[], int d){
+    float n=0;
+    for (int i=0 ; i<d; i++){
+        if (n < num[i]){
+            n=num[i];
+        }
+    }
+    return n;
+}
